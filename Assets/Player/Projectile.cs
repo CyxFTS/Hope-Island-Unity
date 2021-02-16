f5820adef01516b672e8f60ca1ee3edfc410721a
FlyingRope <caiyx@vip.qq.com>
2021-02-15 21:35:36 -0800

    Add Player and Camera Controller

---
+++ Assets/Player/Projectile.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public GameObject FirePoint;
+    public Camera Cam;
+    public float MaxLength;
+    public GameObject[] Prefabs;
+
+    private Ray RayMouse;
+    private Vector3 direction;
+    private Quaternion rotation;
+
+    private int Prefab;
+    private GameObject Instance;
+    private float hSliderValue = 0.1f;
+    private float fireCountdown = 0f;
+
+    void Start()
+    {
+        Prefab = 0;
+    }
+
+    void Update()
+    {
+
+        //Fast shooting
+        if (Input.GetMouseButton(1) && fireCountdown <= 0f)
+        {
+            Instantiate(Prefabs[Prefab], FirePoint.transform.position, FirePoint.transform.rotation);
+            fireCountdown = 0;
+            fireCountdown += hSliderValue;
+        }
+        fireCountdown -= Time.deltaTime;
+
+    }
+}
