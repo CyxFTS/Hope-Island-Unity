0dce0076fcd816f475df4e667983ae083f128dcc
FlyingRope <caiyx@vip.qq.com>
2021-04-17 15:40:28 -0700

    Revert "Merge branch 'main' into Player"
    This reverts commit d0033b79301f7d4fa4a3653515454d288b4e6745, reversing
    changes made to 754ad3a5bb041f8f1ba4db07513ea117c732b9aa.

--- Assets/Scenes/Script/SceneTransition.cs
+++ Assets/Scenes/Script/SceneTransition.cs
@@ -7,13 +7,11 @@ using UnityEngine.SceneManagement;
 public class SceneTransition : MonoBehaviour
 {
     public string sceneToLoad;
-    public GameObject player;
 
     public void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player") && !other.isTrigger)
         {
-            player.GetComponent<PlayerController>().SavePlayerSaveData();
             SceneManager.LoadScene(sceneToLoad);
         }
         
