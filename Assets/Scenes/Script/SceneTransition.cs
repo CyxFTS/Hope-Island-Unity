4fd368908d512a5223a9ca78ccc873432122f5f5
Ruiqi (Stephen) Liu <ruiqi.liu7@gmail.com>
2021-04-21 02:13:51 -0700

    CharacterSelection

--- Assets/Scenes/Script/SceneTransition.cs
+++ Assets/Scenes/Script/SceneTransition.cs
@@ -8,14 +8,22 @@ public class SceneTransition : MonoBehaviour
 {
     public string sceneToLoad;
     public GameObject player;
+    public Boolean selectCharacter;
+    public GameObject characterSelector;
 
     public void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player") && !other.isTrigger)
         {
-            player.GetComponent<PlayerController>().SavePlayerSaveData();
-            SceneManager.LoadScene(sceneToLoad);
+            if (selectCharacter)
+            {
+                characterSelector.SetActive(true);
+            }
+            else
+            {
+                player.GetComponent<PlayerController>().SavePlayerSaveData();
+                SceneManager.LoadScene(sceneToLoad);
+            }
         }
-        
     }
 }
