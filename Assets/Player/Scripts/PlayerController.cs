25a9f51ad8a88239e67bb57177e4f3d8e545946f
FlyingRope <caiyx@vip.qq.com>
2021-04-21 11:28:00 -0700

    Fix Player Stats

--- Assets/Player/Scripts/PlayerController.cs
+++ Assets/Player/Scripts/PlayerController.cs
@@ -111,6 +111,7 @@ public class PlayerController : MonoBehaviour
             return;
 
         sword.Damage = stats.strength.GetCalculatedStatValue();
+        sword.energyCharge = 5f * stats.EnergyRecharge.GetCalculatedStatValue();
         StartAttack();
 
         //staminaSkill = skills.invisibility;
@@ -199,9 +200,9 @@ public class PlayerController : MonoBehaviour
             //Idle
             Idle();
         }
-
+        
         moveDirection *= moveSpeed;
-
+        moveDirection *= (stats.movementSpeed.GetCalculatedStatValue() / 100f);
         controller.Move(moveDirection * Time.deltaTime);
 
         velocity.y += gravity * Time.deltaTime;
