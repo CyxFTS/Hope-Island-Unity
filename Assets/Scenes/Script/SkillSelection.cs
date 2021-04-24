9ad3218296c0dfe96d92eb8bd1a2a1e2e2e0d94a
Ruiqi (Stephen) Liu <ruiqi.liu7@gmail.com>
2021-04-23 18:07:05 -0700

    Skill Selection and Scene Transition Bug Fix

--- Assets/Scenes/Script/SkillSelection.cs
+++ Assets/Scenes/Script/SkillSelection.cs
@@ -93,7 +93,9 @@ public class SkillSelection : MonoBehaviour
                 {
                     newSkill = GetCharlotteStaminaSkill();
                     while (newSkill.description == player.GetComponent<PlayerController>().staminaSkill.description)
+                    {
                         newSkill = GetCharlotteStaminaSkill();
+                    }
                 }
 
                 break;
@@ -111,7 +113,9 @@ public class SkillSelection : MonoBehaviour
                 {
                     newSkill = GetMarcStaminaSkill();
                     while (newSkill.description == player.GetComponent<PlayerController>().staminaSkill.description)
+                    {
                         newSkill = GetMarcStaminaSkill();
+                    }
                 }
                 break;
             case 2:
@@ -128,7 +132,9 @@ public class SkillSelection : MonoBehaviour
                 {
                     newSkill = GetVincentStaminaSkill();
                     while (newSkill.description == player.GetComponent<PlayerController>().staminaSkill.description)
+                    {
                         newSkill = GetVincentStaminaSkill();
+                    }
                 }
                 break;
         }
@@ -172,7 +178,7 @@ public class SkillSelection : MonoBehaviour
     {
         PlayerSkills.BaseSkill skill = player.GetComponent<PlayerSkills>().fireball;
         var random = new System.Random();
-        var skillID = random.Next(0, 7);
+        var skillID = random.Next(0, 3);
         switch (skillID)
         {
             case 0:
